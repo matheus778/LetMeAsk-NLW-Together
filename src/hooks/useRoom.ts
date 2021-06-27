bd7b7b363a07a780422224be0ec140d866daf786
matheus778 <matheuzv777@gmail.com>
2021-06-27 18:40:18 -0300

    adicionado a funcionalidade de fechar sala

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { database } from '../services/firebase';
+import { useAuth } from './useAuth';
 
 type QuestionType = {
   id: string;
@@ -10,6 +11,8 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighLighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 }
 
 type firebaseQuestions = Record<string, {
@@ -20,9 +23,13 @@ type firebaseQuestions = Record<string, {
   content: string;
   isAnswered: boolean;
   isHighLighted: boolean;
+  likes: Record<string, {
+    authorId: string;
+  }>
 }>
 
 export function useRoom(roomId: string) {
+  const { user } =  useAuth()
   const [questions, setQuestion] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -40,15 +47,21 @@ export function useRoom(roomId: string) {
           content: value.content,
           author: value.author,
           isHighLighted: value.isHighLighted,
-          isAnswered: value.isAnswered
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like])=> like.authorId === user?.id)?.[0]
         }
       })
 
       setTitle(databaseRoom.title)
       setQuestion(parsedQuestions);
     });
+    
+    return () => {
+      roomRef.off('value');
+    }
 
-  }, [roomId]);
+  }, [roomId, user?.id]);
 
 
   return {questions, title};
